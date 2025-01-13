263cece761f7033e527078f747d42f13823ca378
Pablo Martínez <91073313+pablomtnez@users.noreply.github.com>
2025-01-13 09:27:42 +0100

    Update generateBooksJson.js

--- microservicioBook/generateBooksJson.js
+++ microservicioBook/generateBooksJson.js
@@ -8,9 +8,9 @@ async function fetchBooks(page, limit) {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
-        q: "books", // Consulta general
-        page: page, // Página actual
-        limit: limit, // Resultados por página
+        q: "books",
+        page: page,
+        limit: limit,
       },
     });
     return response.data.docs;
@@ -20,11 +20,24 @@ async function fetchBooks(page, limit) {
   }
 }
 
+// Limpieza de datos
+function cleanData(book) {
+  return {
+    id: book.key.split("/").pop(),
+    title: book.title || "Unknown",
+    author: book.author_name ? book.author_name[0] : "Unknown",
+    publish_date: book.first_publish_year || "Unknown",
+    pages: isNaN(Number(book.number_of_pages_median)) ? null : Number(book.number_of_pages_median), // Convertir a número o null
+    genre: book.subject ? book.subject[0] : "Unknown",
+    language: book.language ? book.language[0] : "Unknown",
+  };
+}
+
 // Generar archivo JSON con libros
 async function generateBooksJson() {
   const booksDataset = [];
-  const totalBooks = 10000; // Cantidad total de libros que queremos
-  const batchSize = 100; // Tamaño del lote (máximo por solicitud)
+  const totalBooks = 10000;
+  const batchSize = 100;
 
   console.log("Iniciando recopilación de libros...");
 
@@ -36,15 +49,7 @@ async function generateBooksJson() {
 
     books.forEach((book) => {
       if (booksDataset.length < totalBooks) {
-        booksDataset.push({
-          id: booksDataset.length + 1,
-          title: book.title || "Unknown",
-          author: book.author_name ? book.author_name[0] : "Unknown",
-          publish_date: book.first_publish_year || "Unknown",
-          pages: book.number_of_pages_median || "Unknown",
-          genre: book.subject ? book.subject[0] : "Unknown",
-          language: book.language ? book.language[0] : "Unknown",
-        });
+        booksDataset.push(cleanData(book));
       }
     });
   }
