9d0b9c236153901af45a914d44c69ad25cb92007
Pablo Martínez <91073313+pablomtnez@users.noreply.github.com>
2025-01-26 17:28:45 +0100

    Update db.js

--- microservicioBook/config/db.js
+++ microservicioBook/config/db.js
@@ -4,10 +4,11 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/booksDB");
     console.log(`MongoDB conectado: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error conectando a MongoDB: ${error.message}`);
-    process.exit(1);
-  }
-};
+  }catch (error) {
+      console.error(`Error conectando a MongoDB: ${error.message}`);
+      console.error(`Detalles del error:`, error);
+      process.exit(1);
+    }
+  };
 
 module.exports = connectDB;
